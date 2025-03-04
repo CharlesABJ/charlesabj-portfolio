ae9111096f30ffae16b422eb0526431c4a86e49a
Charles ABJ <biadjeu.charles@orange.fr>
2025-03-04 20:02:01 +0100

    test

--- _components/_Contact/Contact.tsx
+++ _components/_Contact/Contact.tsx
@@ -1,6 +1,6 @@
 import { faArrowRight, faComment } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+// import React from 'react';
 import Form from '../Form/Form';
 
 
